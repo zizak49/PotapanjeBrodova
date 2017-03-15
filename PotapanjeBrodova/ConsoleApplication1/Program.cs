e0b21b77e812895f2d717dd6dde29c3e299ed3e0
Mislav Franjo <mislav.franja@outlook.com>
2017-03-15 22:20:43 +0100

    polje

---
+++ PotapanjeBrodova/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Polje : IEquatable<Polje>
+    {
+        public Polje(int redak, int stupac)
+        {
+            Redak = redak;
+            Stupac = stupac;
+        }
+
+        public readonly int Redak;
+        public readonly int Stupac;
+
+        public bool Equals(Polje other)
+        {
+            return Redak == other.Redak && Stupac == other.Stupac;
+        }
+    }
+}
