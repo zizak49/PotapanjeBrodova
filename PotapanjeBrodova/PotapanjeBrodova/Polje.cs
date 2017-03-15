14daa0c7a554d7d81ef54918f7fc88fe16b487de
Julijan Šribar <julijan.sribar@gmail.com>
2017-03-15 21:18:15 +0100

    Riješili smo Polje

---
+++ PotapanjeBrodova/PotapanjeBrodova/Polje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Polje : IEquatable<Polje>
+    {
+        public Polje(int redak, int stupac)
+        {
+            Redak = redak;
+            Stupac = stupac;
+        }
+
+        public readonly int Redak;
+        public readonly int Stupac;
+
+        public bool Equals(Polje other)
+        {
+            return Redak == other.Redak && Stupac == other.Stupac;
+        }
+    }
+}
---
+++ PotapanjeBrodova/Test/TestPolja.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestPolja
+    {
+        [TestMethod]
+        public void Polje_RedakIStupacJednakiSuArgumentimaKonstruktora()
+        {
+            Polje p = new Polje(2, 3);
+            Assert.AreEqual(2, p.Redak);
+            Assert.AreEqual(3, p.Stupac);
+        }
+
+        [TestMethod]
+        public void Polje_ZaDvaPoljaKojaImajuIsteKoordinateMetodaEqualsVraćaTrue()
+        {
+            Polje p1 = new Polje(2, 3);
+            Polje p2 = new Polje(2, 3);
+            Assert.IsTrue(p1.Equals(p2));
+        }
+
+        [TestMethod]
+        public void Polje_ZaDvaPoljaKojaNemajuIsteKoordinateMetodaEqualsVraćaFalse()
+        {
+            Polje p1 = new Polje(2, 3);
+            Polje p2 = new Polje(1, 3);
+            Assert.IsFalse(p1.Equals(p2));
+            Polje p3 = new Polje(2, 4);
+            Assert.IsFalse(p1.Equals(p3));
+        }
+    }
+}
