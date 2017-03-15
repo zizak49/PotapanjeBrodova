7664a14bdf645d4f92e6d255d157e1e3ad1dec36
Mislav Franjo <mislav.franja@outlook.com>
2017-03-15 22:43:24 +0100

    done

--- PotapanjeBrodova/PotapanjeBrodova/Polje.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Polje.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace PotapanjeBrodova
 {
     public class Polje : IEquatable<Polje>
     {
         public Polje(int redak, int stupac)
         {
             Redak = redak;
-            Stupac = stupac;
+            Stupac = stupac;    
         }
 
         public readonly int Redak;
@@ -19,7 +19,7 @@ namespace ConsoleApplication1
 
         public bool Equals(Polje other)
         {
-            return Redak == other.Redak && Stupac == other.Stupac;
+            return Redak == other.Redak && Stupac == other.Stupac; 
         }
     }
 }
--- PotapanjeBrodova/PotapanjeBrodova/Properties/AssemblyInfo.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("PotapanjeBrodova")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("PotapanjeBrodova")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("503271f0-ac86-4fd2-a683-7bd273f26390")]
+[assembly: Guid("50473e1c-79dc-44b5-a3f8-1178c0304e2b")]
 
 // Version information for an assembly consists of the following four values:
 //
