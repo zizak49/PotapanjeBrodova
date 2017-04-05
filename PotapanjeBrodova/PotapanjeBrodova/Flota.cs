d5b784df9a5d726764d5a3e49f4cf1837b2dc6b3
Julijan Šribar <julijan.sribar@gmail.com>
2017-04-05 20:46:40 +0200

    Napravljena Flota i testovi.

---
+++ PotapanjeBrodova/PotapanjeBrodova/Flota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Flota
+    {
+        public void DodajBrod(IEnumerable<Polje> polja)
+        {
+            brodovi.Add(new Brod(polja));
+        }
+
+        public int BrojBrodova
+        {
+            get { return brodovi.Count; }
+        }
+
+        private List<Brod> brodovi = new List<Brod>();
+    }
+}
---
+++ PotapanjeBrodova/Test/TestFlote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestFlote
+    {
+        [TestMethod]
+        public void Flota_DodajBrodPovećavaFlotuZaJedanBrod()
+        {
+            Flota flota = new Flota();
+            Assert.AreEqual(0, flota.BrojBrodova);
+            flota.DodajBrod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            Assert.AreEqual(1, flota.BrojBrodova);
+        }
+    }
+}
