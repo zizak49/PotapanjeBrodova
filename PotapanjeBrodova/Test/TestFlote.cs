26f6ca0d80a5f3a245bbcd8e79e15411a7914ea4
Mislav Franjo <mislav.franja@gmail.com>
2017-04-05 20:47:26 +0200

    flota gotova

---
+++ PotapanjeBrodova/Test/TestFlote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
+namespace Test
+{
+    [TestClass]
+    public class TestFlote
+    {
+        [TestMethod]
+        public void Flota_DodajBrodPovecavaFlotuZaJedanBrod()
+        {
+            Flota flota = new Flota();
+            Assert.AreEqual(0, flota.BrojBrodova);
+            flota.DodajBrod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            Assert.AreEqual(1, flota.BrojBrodova);
+        }
+    }
+}
