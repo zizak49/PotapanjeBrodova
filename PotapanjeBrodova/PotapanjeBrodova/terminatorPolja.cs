13a1e29eeffd6ea4a669261067249fd5c8f2a940
Mislav Franjo <mislav.franja@gmail.com>
2017-04-05 21:11:36 +0200

    gotovi

---
+++ PotapanjeBrodova/PotapanjeBrodova/terminatorPolja.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+     public class terminatorPolja
+    {
+        public terminatorPolja(Mreža mreža)
+        {
+            this.mreža = mreža;
+        }
+        public void UkloniPolja(IEnumerable<Polje> polja)
+        {
+            //
+            //mreže.UkloniPolje(r, s);
+        }
+        private Mreža mreže;
+    }
+}
