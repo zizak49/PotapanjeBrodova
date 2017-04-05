3a8fa66b930950268258b36aae77507c882c0230
Julijan Šribar <julijan.sribar@gmail.com>
2017-04-05 20:36:42 +0200

    Dodan Brod i test

---
+++ PotapanjeBrodova/Test/TestBroda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class TestBroda
+    {
+        [TestMethod]
+        public void Brod_SadržiPoljaZadanaKonstruktorom()
+        {
+            Brod brod = new Brod(new Polje[] { new Polje(1, 1), new Polje(1, 2) });
+            Assert.AreEqual(2, brod.Polja.Count());
+            Assert.IsTrue(brod.Polja.Contains(new Polje(1, 1)));
+            Assert.IsTrue(brod.Polja.Contains(new Polje(1, 2)));
+        }
+    }
+}
