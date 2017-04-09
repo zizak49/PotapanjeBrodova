bbcbf7515425867a5c1bc52d513ceddafe655196
Julijan Šribar <julijan.sribar@gmail.com>
2017-04-09 23:58:59 +0200

    Dodano sortiranje polja i testovi

---
+++ PotapanjeBrodova/PotapanjeBrodova/Proširenja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public static class Proširenja
+    {
+        public static IEnumerable<Polje> Sortiraj(this IEnumerable<Polje> polja)
+        {
+            if (polja == null || polja.Count() <= 1)
+                return polja;
+            Polje prvo = polja.First();
+            bool uspravnoPoravnata = polja.Skip(1).All(p => p.Stupac == prvo.Stupac);
+            bool vodoravnoPoravnata = polja.Skip(1).All(p => p.Redak == prvo.Redak);
+            if (vodoravnoPoravnata == uspravnoPoravnata)
+                throw new ArgumentException();
+            return polja.OrderBy(p => p.Redak + p.Stupac);
+        }
+    }
+}
---
+++ PotapanjeBrodova/Test/TestSortiranjaPolja.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class TestSortiranjaPolja
+    {
+        [TestMethod]
+        public void SortirajPolja_SortiraNizHorizontalnihPoljaOdNajlijevijegPremaNajdesnijem()
+        {
+            Polje[] polja = new Polje[] { new Polje(2, 3), new Polje(2, 4), new Polje(2, 2) };
+            IEnumerable<Polje> sortirana = polja.Sortiraj();
+            Assert.AreEqual(new Polje(2, 2), sortirana.ElementAt(0));
+            Assert.AreEqual(new Polje(2, 3), sortirana.ElementAt(1));
+            Assert.AreEqual(new Polje(2, 4), sortirana.ElementAt(2));
+        }
+
+        [TestMethod]
+        public void SortirajPolja_SortiraNizVertikalnihPoljaOdNajgornjegPremaNajdonjem()
+        {
+            Polje[] polja = new Polje[] { new Polje(4, 3), new Polje(5, 3), new Polje(3, 3) };
+            IEnumerable<Polje> sortirana = polja.Sortiraj();
+            Assert.AreEqual(new Polje(3, 3), sortirana.ElementAt(0));
+            Assert.AreEqual(new Polje(4, 3), sortirana.ElementAt(1));
+            Assert.AreEqual(new Polje(5, 3), sortirana.ElementAt(2));
+        }
+
+        [TestMethod]
+        public void SortirajPolja_ZaProsljeđenuNulReferencuVraćaNulReferencu()
+        {
+            Polje[] polja = null;
+            Assert.IsNull(polja.Sortiraj());
+        }
+
+        [TestMethod]
+        public void SortirajPolja_ZaNizKojiSeSastojiOdJednogPoljaVraćaTajNiz()
+        {
+            Polje[] polja = new Polje[] { new Polje(2, 3) };
+            Assert.AreEqual(1, polja.Sortiraj().Count());
+            Assert.IsTrue(polja.Sortiraj().Contains(new Polje(2, 3)));
+        }
+
+        [TestMethod]
+        public void SortirajPolja_BacaIznimkuTipaArgumentExceptionZaNizPoljaKojaNisuPoravnataHorizontalnoIliVertikalno()
+        {
+            try
+            {
+                Polje[] polja = new Polje[] { new Polje(4, 3), new Polje(5, 4) };
+                polja.Sortiraj();
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
