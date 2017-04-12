81000ff2b1d8ed12bb3426f677085922b9661a93
Mislav Franjo <mislav.franja@gmail.com>
2017-04-12 18:06:59 +0200

    Implementirana metoda UkloniPolja

--- PotapanjeBrodova/PotapanjeBrodova/terminatorPolja.cs
+++ PotapanjeBrodova/PotapanjeBrodova/terminatorPolja.cs
@@ -13,8 +13,14 @@ namespace PotapanjeBrodova
         }
         public void UkloniPolja(IEnumerable<Polje> polja)
         {
-            //
-            //mreže.UkloniPolje(r, s);
+            IEnumerable<Polje> sortirana = polja.Sortiraj();
+            int r0 = Math.Max(sortirana.First().Redak - 1, 0);
+            int s0 = Math.Max(sortirana.First().Stupac - 1, 0);
+            int r1 = Math.Min(sortirana.Last().Redak + 2, mreža.Redaka);
+            int s1 = Math.Min(sortirana.Last().Stupac + 2, mreža.Stupaca);
+            for (int r = r0; r < r1; ++r)
+                for (int s = s0; s < s1; ++s)
+                    mreža.UkloniPolje(r, s);
         }
         private Mreža mreža;
     }
