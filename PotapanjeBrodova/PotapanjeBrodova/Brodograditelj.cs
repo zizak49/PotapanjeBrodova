27c33581ff1da8ba325c62128f95bab7625ac85f
Mislav Franjo <mislav.franja@gmail.com>
2017-04-12 19:51:42 +0200

    gotovi sa brodograditeljom

---
+++ PotapanjeBrodova/PotapanjeBrodova/Brodograditelj.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Brodograditelj
+    {
+        public Flota SloziFlotu(Mreža mreža,IEnumerable<int> duljineBrodova)
+        {
+            Flota flota = new Flota();
+            TerminatorPolja terminator = new TerminatorPolja(mreža);
+
+            foreach (int i in duljineBrodova) {
+                var nizovi = mreža.DajNizoveSlobodnihPolja(i);
+                
+                int index= slučajni.Next(nizovi.Count());
+                var niz=nizovi.ElementAt(index);
+                flota.DodajBrod(niz);
+                terminator.UkloniPolja(niz);
+            }
+            return flota;
+         
+        }
+        // todo: obrati paznju da se nemugu svi brodovi sloziti
+        private Random slučajni = new Random();
+
+    }
+}
---
+++ PotapanjeBrodova/Test/TestBrodograditelja.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class TestBrodograditelja
+    {
+        [TestMethod]
+        public void Brodograditelj_SloziFlotuVracaFlotuS_BrodovimaZadaneDuljine()
+        {
+
+            Brodograditelj b = new Brodograditelj();
+            Mreža mreža = new Mreža(10, 10);
+            IEnumerable<int> duljineBrodova = new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+
+            Flota f = b.SloziFlotu(mreža, duljineBrodova);
+            Assert.AreEqual(duljineBrodova.Count(), f.BrojBrodova);
+            // TODO: provjera ima li samo jedan broda duljine 5
+            // TODO: provjera ima li samo dva broda duljine 4
+            // TODO: provjera ima li samo tri broda duljine 3
+
+
+
+        }
+    }
+}
